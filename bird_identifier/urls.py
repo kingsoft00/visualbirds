c84e25ae0114968459402a00c80ad313549bd449
Tyler Kramlich <kramlichtyler@gmail.com>
2019-10-20 20:36:57 -0400

    Fix urlpattern
    Switched 'input/' to 'results/'

--- bird_identifier/urls.py
+++ bird_identifier/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
-    path('input/', views.results, name='results')
+    path('results/', views.results, name='results')
 ]
