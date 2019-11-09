78717f0b42e934a07df05ee722755a369b0641af
Kramlich <kramlichtyler@gmail.com>
2019-11-09 11:33:35 -0500

    Add parsing functions

--- bird_identifier/views.py
+++ bird_identifier/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from .forms import UploadFileForm
 from django.http import HttpResponse
 import json
+from django.contrib.messages import get_messages
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
@@ -22,15 +23,50 @@ def getResponseFromWatson(file):
     
     return classes
 
-def parseWatsonResponse(classes):
+def getConfidenceLevels(classes):
     print(json.dumps(classes, indent=2))
+    
+    classifiers = classes["images"][0]
+    classifiers_JSON = classifiers['classifiers'][0]
+    confidence_levels = classifiers_JSON["classes"]
+    
+    return confidence_levels      
+
+def separateConfidenceLevelsIntoWorkable(confidence_levels):
+
+    list_of_confidence_ratings = []
+
+    for n in confidence_levels:
+        for x in n.items():
+
+            if x[0] == "class":
+                bird_class = x[1]
+            if x[0] == "score":
+                confidence_score = x[1]
+
+        confidence_rating = (bird_class, confidence_score)
+        list_of_confidence_ratings.append(confidence_rating)
+
+    return list_of_confidence_ratings
+
+def determineHighestConfidenceRating(confidence_rating):
+
+    maxRating = 0
+    bird_class = ""
+
+    for x in confidence_rating:
+        if (x[1] > maxRating):
+            maxRating = x[1]
+            bird_class = x[0]
+    
+    return (bird_class, maxRating) 
 
 # Main page to be templated
 def index(request):
     return render(request, 'bird_identifier/information.html')
 
 # Contains results
-def results(request):
+def results(request, highest_score):
     return render(request, 'bird_identifier/results.html')
 
 def success(request):
@@ -44,9 +80,14 @@ def upload(request):
         
         if form.is_valid():
             instance = ModelWithFileField(user_photo=request.FILES['file'])
+
             classes = getResponseFromWatson(instance.user_photo)
-            parseWatsonResponse(classes)
-            return redirect('results')
+            confidence_levels = getConfidenceLevels(classes)
+            confidence_ratings = separateConfidenceLevelsIntoWorkable(confidence_levels)
+            highest_score = determineHighestConfidenceRating(confidence_ratings)
+
+
+            return results(request, highest_score)
 
     else:
         form = UploadFileForm()
